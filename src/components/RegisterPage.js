2d297d7d6ef454c5117110cbc6cf8a9c06982292
Enoch Nyako <enochnyako@gmail.com>
2019-09-05 12:34:56 +0000

    connect event listeners to state on register page

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -23,34 +23,42 @@ export class RegisterPage extends Component {
 
     handleChange(event) {
         // handle input change and dispatch register
+        const { user } = this.state;
+
+        this.setState({user: {...user, [event.target.name]: event.target.value }});
     }
 
     handleSubmit(event) {
         // handle button click and dispatch register
+        if(event) event.preventDefault();
+        console.log("dispatch register");
+        const { user: {username, password}} = this.state;
+
     }
 
     render() {
         const { user, submitted } = this.state;
+        const {username, password} = user;
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Register</h2>
                 <form name="form">
                     <div className={'form-group' + (submitted && !user.username ? ' has-error' : '')}>
                         <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control username" name="username" />
+                        <input type="text" className="form-control username" name="username" value={username} onChange={this.handleChange} />
                         {submitted && !user.username &&
                             <div className="help-block">Username is required</div>
                         }
                     </div>
                     <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password"/>
+                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
                         {submitted && !user.password &&
                             <div className="help-block">Password is required</div>
                         }
                     </div>
                     <div className="form-group">
-                        <button className="btn btn-primary">Register</button>
+                        <button type="submit" onClick={this.handleSubmit} className="btn btn-primary">Register</button>
                         <Link to="/login" className="btn btn-link">Cancel</Link>
                     </div>
                 </form>
@@ -61,7 +69,9 @@ export class RegisterPage extends Component {
 
 // complete the below function
 function mapStateToProps(state) {
-    
+    console.log(state);
+    return {};
 }
 
 export { RegisterPage as TestRegisterPage };
+// export default connect(mapStateToProps, null)(RegisterPage);
